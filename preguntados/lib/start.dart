de81ce2b656c0658057f83af6a9f52acfefa2856
santiago lopez de haro <santiagolhgpro@gmail.com>
2024-04-25 14:36:44 -0300

    Termine de diseñar la ventana principal del preguntados

---
+++ preguntados/lib/start.dart
@@ -0,0 +1,32 @@
+import "package:flutter/material.dart";
+
+class Start extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset("assets/images/quiz-logo.png", width: 300),
+        const SizedBox(
+          height: 40,
+        ),
+        const Text(
+          "Learn Flutter the fun way !",
+          style: TextStyle(
+              fontSize: 24, color: Color.fromARGB(196, 171, 169, 177)),
+        ),
+        const SizedBox(
+          height: 66,
+        ),
+        OutlinedButton(
+          onPressed: () {},
+          child: const Text(
+            "Start Quiz",
+            style: TextStyle(
+                fontSize: 15, color: Color.fromARGB(196, 171, 169, 177)),
+          ),
+        ),
+      ],
+    );
+  }
+}
