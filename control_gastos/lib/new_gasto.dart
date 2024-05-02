1e9d6c2a7fc4009831b3244de13bd76deb47b9d4
santiago lopez de haro <santiagolhgpro@gmail.com>
2024-05-01 21:51:48 -0300

    Agregue un selector de calendario

--- control_gastos/lib/new_gasto.dart
+++ control_gastos/lib/new_gasto.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class NewGasto extends StatefulWidget
-{
+class NewGasto extends StatefulWidget {
   const NewGasto({super.key});
 
   @override
@@ -10,32 +10,70 @@ class NewGasto extends StatefulWidget
   }
 }
 
-class _NewGastoState extends State<NewGasto>
-{
-  String newTitulo = "";
-  void _saveTitulo(String titulo)
-  {
-    newTitulo = titulo;
-  }
+class _NewGastoState extends State<NewGasto> {
+  final _controladorTitulo = TextEditingController();
+  final _controladorCant = TextEditingController();
+
   @override
-  Widget build(BuildContext context) 
+  void dispose() {
+    _controladorTitulo.dispose();
+    _controladorCant.dispose();
+    super.dispose();
+  }
+
+  void _mostrarCalendario()
   {
+    final now = DateTime.now();
+    showDatePicker(context: context, firstDate: DateTime(now.year, now.month, 1), lastDate: now);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(16),
-      child: Column(children: [
-        TextField(
-          onChanged: _saveTitulo,
-          maxLength: 30,
-          decoration: const InputDecoration(
-            label: Text("Titulo"),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _controladorTitulo,
+            maxLength: 30,
+            decoration: const InputDecoration(
+              label: Text("Titulo"),
+            ),
           ),
-        ),
-        Row(
-          children: [
-            ElevatedButton(onPressed: () {print(newTitulo);}, child: const Text("Guardar gasto")),
-          ],
-        )
-      ],),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _controladorCant,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefixText: "UY\$",
+                    label: Text("cantidad"),
+                  ),
+                ),
+              ),
+              Expanded(child: 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                Text("Selected Date"),
+                IconButton(onPressed: _mostrarCalendario, icon: const Icon(Icons.calendar_month)),
+              ],))
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(onPressed: () {Navigator.pop(context);}, child: const Text("Cancelar")),
+              ElevatedButton(
+                  onPressed: () {
+                    print(
+                        "${_controladorTitulo.text} : ${_controladorCant.text}");
+                  },
+                  child: const Text("Guardar gasto")),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
