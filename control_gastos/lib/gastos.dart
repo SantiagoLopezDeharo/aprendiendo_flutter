daea71ff2930c25ce906abf4152fcaf92d73a6bc
santiago lopez de haro <santiagolhgpro@gmail.com>
2024-05-02 18:48:43 -0300

    Agregue la posibilidad de deshacer una eliminacion con un pop up

--- control_gastos/lib/gastos.dart
+++ control_gastos/lib/gastos.dart
@@ -31,9 +31,24 @@ class _Gastos extends State<Gastos> {
   }
 
   void _removeGasto(Gasto gasto) {
+
+    final index = _gastosRegistrados.indexOf(gasto);
     setState(() {
       _gastosRegistrados.remove(gasto);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: const Text("Gasto eliminado"),
+      duration: const Duration(seconds: 3),
+      action: SnackBarAction(
+        label: "Deshacer",
+        onPressed: () {
+          setState(() {
+            _gastosRegistrados.insert(index, gasto);
+          });
+        },),
+      )
+      );
   }
 
   void _abrirAddGastoOverlay() {
@@ -46,6 +61,15 @@ class _Gastos extends State<Gastos> {
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(child: Text("No se encontraron gastos."),);
+    if(_gastosRegistrados.isNotEmpty)
+    {
+      mainContent = ListaGastos(
+              gastos: _gastosRegistrados,
+              onRemoveGasto: _removeGasto,
+            );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -63,10 +87,8 @@ class _Gastos extends State<Gastos> {
           children: [
             const Text("Las graficas"),
             Expanded(
-                child: ListaGastos(
-              gastos: _gastosRegistrados,
-              onRemoveGasto: _removeGasto,
-            )),
+                child: mainContent,
+            ),
           ],
         ),
       ),
