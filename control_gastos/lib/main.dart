2a469856365e0892063bbcf5f4ea0b914971f07e
santiago lopez de haro <santiagolhgpro@gmail.com>
2024-05-02 23:55:56 -0300

    Aprendi a ajustar el tema de colores de la app y crear una version diferente para el modo obscuro y claro del systema

--- control_gastos/lib/main.dart
+++ control_gastos/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:control_gastos/gastos.dart';
 
+var KColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 144, 227, 167));
+var KDarkColorScheme = ColorScheme.fromSeed(brightness: Brightness.dark, seedColor: const Color.fromARGB(255, 47, 70, 54));
 void main() {
   runApp(const MyApp());
 }
@@ -12,7 +15,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Gastos()
-    );
+        darkTheme: ThemeData.dark().copyWith(
+          colorScheme: KDarkColorScheme,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: KDarkColorScheme.primaryContainer,
+            ),
+          )
+        ),
+        theme: ThemeData().copyWith(
+          colorScheme: KColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: KColorScheme.onSecondaryContainer,
+            foregroundColor: KColorScheme.primaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: KColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 9),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: KColorScheme.primaryContainer,
+            ),
+          ),
+        ),
+        themeMode: ThemeMode.system,
+        home: Gastos());
   }
 }
